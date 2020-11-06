e8da5c104ba7255c02e5389ea1e062dd4920fffb
CJ Faulk <cjfaulk@ufl.edu>
2020-11-06 11:44:56 -0500

    Complete part ii - print 32

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
-// Print 212 using an arrow function
+// Part i - Print 212 using an arrow function
 
 var boilingF;
 boilingF = () => "212";
 document.getElementById("boiling-temp").innerHTML = boilingF();
+
+
+// Part ii - Print 32 using an arrow function
+
+var freezingF;
+freezingF = () => "32";
+document.getElementById("freezing-temp").innerHTML = freezingF();
